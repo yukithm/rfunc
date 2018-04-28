9cb487a279c7e7d3a849db01cb0966ec997cee51
Yuki <yukithm@gmail.com>
2018-04-28 19:47:11 +0900

    Add open command

---
+++ cmd/open.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/yukithm/rfunc/client"
+)
+
+var openCmd = &cobra.Command{
+	Use:   "open",
+	Short: "Open specified URL on the server",
+	RunE:  runOpenCmd,
+}
+
+func init() {
+	rootCmd.AddCommand(openCmd)
+}
+
+func runOpenCmd(cmd *cobra.Command, args []string) error {
+	return client.RunRFunc(globalOpts.Network(), globalOpts.Address(), func(rfunc *client.RFunc) error {
+		return rfunc.OpenURL(args...)
+	})
+}

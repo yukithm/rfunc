9b3b5178e3a4b96f351f635faf6e0caa8169154c
Yuki <yukithm@gmail.com>
2018-04-29 00:41:13 +0900

    Add --daemon option to server command

--- cmd/server.go
+++ cmd/server.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"github.com/VividCortex/godaemon"
 	"github.com/spf13/cobra"
 	"github.com/yukithm/rfunc/server"
 )
@@ -11,11 +12,22 @@ var serverCmd = &cobra.Command{
 	RunE:  runServerCmd,
 }
 
+var serverOpts = struct {
+	Daemon bool
+}{}
+
 func init() {
+	f := serverCmd.Flags()
+	f.BoolVar(&serverOpts.Daemon, "daemon", serverOpts.Daemon, "daemonize")
+
 	rootCmd.AddCommand(serverCmd)
 }
 
 func runServerCmd(cmd *cobra.Command, args []string) error {
+	if serverOpts.Daemon {
+		godaemon.MakeDaemon(&godaemon.DaemonAttr{})
+	}
+
 	lis, err := server.NewServerConn(globalOpts.Network(), globalOpts.Address())
 	if err != nil {
 		return err
